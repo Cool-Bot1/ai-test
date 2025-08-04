96cfb9552b5934e3f9c6e778ed4712e95578b901
Marco <marco.ergueta@csedge.org>
2025-08-04 14:37:55 -0700

    i question reality

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html",scr=scr,img_src=img_src,img_num=img_num)
+    return render_template("index.html",scr=scr,img_src=img_src,img_num=img_num,img=img)
 
 @app.route("/",methods=['POST'])
 def clear():
     global scr
+    global final
+    global img
     if scr==10:
         final=True
-    return render_template ("index.html",explanation=False,ans=False,scr=scr,img_src=img_src,img_num=img_num,final=final)
+    img+=1
+    return render_template ("index.html",explanation=False,ans=False,scr=scr,img_src=img_src,img_num=img_num,final=final,img=img)
 
 scr=int(0)
 used=[False,False,False,False,False,False,False,False,False,False]
 
-img_list=[False,True,True,True,True,True,True,True,True,True]
+img_list=[False,False,False,True,True,True,True,True,True,True]
 explain=[
-    "1",
-    "2",
-    "3",
-    "4",
+    "This is a photo of a Flamingo from a strange angle. It was submitted in an AI image generation competion and won, however its creator revealed it was real",
+    "This is a Jerboa, if you thought this is AI you probably hurt that little goober's feelings",
+    "This is a Proboscis Monkey, he looks a bit like squidward",
+    "This is a Saiga Antelope it lives in Russia and is on the verge of extinction (I will say the lighting is a somewhat similar to AI)",
     "5",
     "6",
     "7",
@@ -29,10 +32,21 @@ explain=[
     "10",
 ]
 img_num= -1
+img=0
 img_src=[
     "static/img_1.jpeg",
-
+    "static/img_2.png",
+    "static/img_3.jpg",
+    "static/img_4.webp",
+    "static/img_5.png",
+    "static/img_6.jpeg",
+    "static/img_7.jpg",
+    "static/img_8.jpg",
+    "static/img_9.jpg",
+    
 ]
+
+final=False
 
 def check(choice,x):
     global scr
@@ -52,11 +66,12 @@ def check(choice,x):
             ans="You're wrong its's AI"
         elif choice==True:
             ans="You're wrong its's real"
-    return render_template ("index.html",explanation =explain[x],ans=ans,scr=scr,img_src=img_src,img_num=img_num)
+    return render_template ("index.html",explanation =explain[x],ans=ans,scr=scr,img_src=img_src,img_num=img_num,img=img)
 
 @app.route("/1",methods=['POST'])
 def respond():
     global img_num
+    global final
     img_num+=1
     if img_num!=10:
         if 'AI' in request.form.values():
